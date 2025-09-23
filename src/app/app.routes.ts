60f3cf2dd2d0af30e4c20e3cab5f1452c160f64c
Eduardo <elozano2a@gmail.com>
2025-09-23 17:17:58 -0500

    Basic component, routing, and styling creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Movies } from './movies/movies';
+import { Profile } from './profile/profile';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'movies', component: Movies },
+    { path: 'profile', component: Profile },
+    { path: '**', redirectTo: '/home' }, //Should be not found page in future
+
+];
